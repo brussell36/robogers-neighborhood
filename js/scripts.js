6303c2baae76fcc2dbfb42d79c62ff2fbabcc5cf
Ben Russell <benjaminrussell36@gmail.com>
2020-06-12 14:10:51 -0700

    Add more comparators to if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,13 @@ $(document).ready(function() {
 function roboCount(number) {
   let numberArray = [];
   for (let i = 0; i <= number; i++) {
-    if ([i].includes(3)) {
+    if ([i].includes(3) || i === 13 || i >= 30 && i < 40) {
       numberArray.push(" Won't you be my neighbor?")
     }
-    else if ([i].includes(2)) {
+    else if ([i].includes(2) || i >= 20 && i < 30) {
       numberArray.push(" Boop!")
     }
-    else if ([i].includes(1)) {
+    else if ([i].includes(1) || i >= 10 && i < 20) {
       numberArray.push(" Beep!")
     }
     else {
