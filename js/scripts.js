8e1b064d95285bf2d05a183b78402d90261bb40c
Ben Russell <benjaminrussell36@gmail.com>
2020-06-12 09:23:40 -0700

    Add roboCount function to count up to user input number.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,17 @@
 $(document).ready(function() {
   $("form#robot").submit(function() {
     event.preventDefault();
-    const number = parseInt($("#number").val());
-    const result = 
+    const number = $("#number").val();
+    const result = roboCount(number);
     $("#result").text(result);
   });
 });
 
 // Business Logic
+function roboCount(number) {
+  const numberArray = []
+  for (i = 0; i <= number; i++) {
+    numberArray.push(i);
+  }
+  return result = numberArray;
+}
