d70dfbc832e53055849792bce22654108e6bea88
Ben Russell <benjaminrussell36@gmail.com>
2020-06-12 08:37:34 -0700

    Add sections for UI and Business Logic. Add jQuery to take input from user and save in variable.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// UI logic
+$(document).ready(function() {
+  $("form#robot").submit(function() {
+    event.preventDefault();
+    const number = parseInt($("#number").val());
+    const result = 
+    $("#result").text(result);
+  });
+});
+
+// Business Logic
