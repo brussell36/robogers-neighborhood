3d2620dc6923109672563bfa0c444f53037eade5
Ben Russell <benjaminrussell36@gmail.com>
2020-06-12 15:56:28 -0700

    Initialize new variable of empty string in for loop so it wont stop at first number that includes my search parameters.

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,14 @@ $(document).ready(function() {
 function roboCount(number) {
   let numberArray = [];
   for (let i = 0; i <= number; i++) {
-    if ([i].includes(3) || i === 13 || i >= 30 && i < 40 || i === 43 || i === 53) {
+    let iString = i + "";
+    if (iString.includes(3)) {
       numberArray.push(" Won't you be my neighbor?")
     }
-    else if ([i].includes(2) || i >= 20 && i < 30 || i === 42 || i === 52) {
+    else if (iString.includes(2)) {
       numberArray.push(" Boop!")
     }
-    else if ([i].includes(1) || i >= 10 && i < 20 || i === 41 || i === 51) {
+    else if (iString.includes(1)) {
       numberArray.push(" Beep!")
     }
     else {
